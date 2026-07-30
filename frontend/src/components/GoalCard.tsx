a47089bb2a7cc40cbadaf56384f28704cff35c7c
Shaneel Kumar <shaneel_kumar@live.com>
2026-07-30 21:22:41 +1000

    add transactions to goal data and use calculated values within goal card component

--- frontend/src/components/GoalCard.tsx
+++ frontend/src/components/GoalCard.tsx
@@ -3,20 +3,26 @@ import { Card, CardHeader, CardContent, CardFooter } from "./ui/card"
 import type { Goal } from "#/types"
 
 export const GoalCard = ({ index, goal }: { index: number, goal: Goal }) => {
-	const { name, goalAmount, deadline } = goal
-	// const progressValue = Math.floor(currentAmount / goalAmount * 100)
-	const progressValue = 70
+	const { name, goalAmount, deadline, transactions } = goal
 
-	let cardClass;
+	const currentAmount = transactions ? transactions.reduce((total, transaction) => {
+		if (transaction.type === "deposit") return total + transaction.amount
+			else if (transaction.type === "withdrawal") return total - transaction.amount
+		return total
+	}, 0) : 0;
+
+	const progressValue = Math.floor(currentAmount / goalAmount * 100)
+
+	let cardStyling;
 	if ([0, 5].includes(index)) {
-		cardClass = 'md:col-span-2'
+		cardStyling = 'md:col-span-2'
 	} else if ([1, 4].includes(index)) {
-		cardClass = 'lg:row-span-2'
+		cardStyling = 'lg:row-span-2'
 	}
-	cardClass += [0, 5].includes(index) ? '  bg-orange' : ' bg-zinc-800'
+	cardStyling += [0, 5].includes(index) ? '  bg-orange' : ' bg-zinc-800'
 
 	return (
-		<Card className={cardClass}>
+		<Card className={cardStyling}>
 			<CardHeader>{name}</CardHeader>
 
 			<CardContent>
@@ -25,7 +31,7 @@ export const GoalCard = ({ index, goal }: { index: number, goal: Goal }) => {
 			</CardContent>
 
 			<CardFooter className='flex items-center gap-4 text-xs'>
-				<p><span>CurrentAmount</span> of <span>${goalAmount}</span></p>
+				<p><span>${currentAmount}</span> of <span>${goalAmount}</span></p>
 				<span>{deadline ? 'Due ' + deadline : 'No deadline'}</span>
 			</CardFooter>
 		</Card>
--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,6 +1,14 @@
+export type Transaction = {
+	id?: number
+	amount: number
+	type: "deposit" | "withdrawal"
+	date: string
+}
+
 export type Goal = {
 	id?: number
 	name: string
 	goalAmount: number
 	deadline?: string
+	transactions?: Transaction[]
 }
