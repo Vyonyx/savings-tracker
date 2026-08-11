a8666def984efbdd5fdc12b012a2c4bf35e4978f
Shaneel Kumar <shaneel_kumar@live.com>
2026-08-11 22:35:51 +1000

    move handleInputChange fn to utils and change GoalForm to use this fn

--- frontend/src/components/GoalForm.tsx
+++ frontend/src/components/GoalForm.tsx
@@ -1,9 +1,10 @@
-import { useState, type ChangeEvent } from "react"
+import { useState } from "react"
 import { Button } from "./ui/button"
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card"
 import { FieldSet, FieldGroup, Field, FieldLabel } from "./ui/field"
 import { Input } from "./ui/input"
 import type { Goal, NewGoal } from "#/types"
+import { handleInputChange } from "#/lib/utils"
 
 type Props = {
 	heading: string
@@ -18,14 +19,6 @@ function GoalForm({ heading, goal, submitText }: Props) {
 		deadline: goal?.deadline ?? "",
 	})
 
-	const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-		const id = e.target.id
-		const value = e.target.value
-		setNewGoal((prev) => {
-			return {...prev, [id]: value}
-		})
-	}
-
   return (
 			<Card className='w-full lg:w-6/12'>
 				<CardHeader>
@@ -39,17 +32,17 @@ function GoalForm({ heading, goal, submitText }: Props) {
 								<FieldGroup>
 									<Field>
 										<FieldLabel htmlFor='name'>Name</FieldLabel>
-										<Input id='name' type="text" placeholder="Name" value={newGoal.name} onChange={handleChange} />
+										<Input id='name' type="text" placeholder="Name" value={newGoal.name} onChange={(e) => handleInputChange(e, setNewGoal)} />
 									</Field>
 
 									<Field>
 										<FieldLabel htmlFor='goalAmount'>Goal Amount</FieldLabel>
-										<Input id='goalAmount' type="text" placeholder="0" value={newGoal.goalAmount} onChange={handleChange} />
+										<Input id='goalAmount' type="text" placeholder="0" value={newGoal.goalAmount} onChange={(e) => handleInputChange(e, setNewGoal)} />
 									</Field>
 
 									<Field>
 										<FieldLabel htmlFor='deadline'>Deadline</FieldLabel>
-										<Input id='deadline' type="text" placeholder="2026-01-02" value={newGoal.deadline} onChange={handleChange} />
+										<Input id='deadline' type="text" placeholder="2026-01-02" value={newGoal.deadline} onChange={(e) => handleInputChange(e, setNewGoal)} />
 									</Field>
 
 									<Field className='w-40 mx-auto mt-4'>
--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,6 +1,7 @@
 import type { Transaction } from '#/types'
 import type { ClassValue } from 'clsx'
 import { clsx } from 'clsx'
+import type { ChangeEvent, SetStateAction } from 'react'
 import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
@@ -14,3 +15,11 @@ export const calculateCurrentAmountFromTransactions = (transactions: Transaction
 		return total
 	}, 0) : 0;
 } 
+
+	export const handleInputChange = <T>(e: ChangeEvent<HTMLInputElement>, setFn: React.Dispatch<SetStateAction<T>>) => {
+		const id = e.target.id
+		const value = e.target.value
+		setFn((prev) => {
+			return {...prev, [id]: value}
+		})
+	}
