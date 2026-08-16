58971375eb2e213dade420fca5f75f5673f16d40
Shaneel Kumar <shaneel_kumar@live.com>
2026-08-16 16:23:28 +1000

    scaffold basic golang server

---
+++ backend/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/vyonyx/savings-tracker/backend/initializers"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+}
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	router.Run(os.Getenv("PORT"))
+}
